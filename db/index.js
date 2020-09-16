9848e64a6072269e9f68002a4c0b81aa8aaa8517
Trang Nguyen <ShellyNguyen98@gmail.com>
2020-09-15 19:18:46 -0700

    update

--- db/index.js
+++ db/index.js
@@ -1,4 +1,4 @@
 module.exports = require('mongoose').connect(process.env.MONGODB_URI || 'mongodb://localhost/bud_db', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true 
 })
